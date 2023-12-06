4d8e4796fb5f92e67f9a67a9f9ee66beda21247c
victorandres20 <vapedraza1706@gmail.com>
2023-12-06 11:16:27 -0500

    fix date

--- src/pages/content/inbox/general_detail.js
+++ src/pages/content/inbox/general_detail.js
@@ -1,12 +1,13 @@
 import { Col, Row } from "react-bootstrap";
 
 export default function GeneralDetail({ demand, children }) {
+    const date_created = new Date(demand?.date_created);
     return(
         <Row>
             <Col lg={12}>
                 <div className="flex-row flex-center">
-                    <div style={{ margin: '0 5px', fontSize: '0.7em', fontWeight: 'bold' }}>{ demand?.date_created?.split("T")[0] }</div>
-                    <div style={{ margin: '0 5px', fontSize: '0.7em', fontWeight: 'bold' }}>{ demand?.date_created?.split("T")[1].replace("Z", "") }</div>
+                    <div style={{ margin: '0 5px', fontSize: '0.7em', fontWeight: 'bold' }}>{ `${date_created?.getFullYear()}-${date_created?.getMonth()}-${date_created?.getDate()}` }</div>
+                    <div style={{ margin: '0 5px', fontSize: '0.7em', fontWeight: 'bold' }}>{ `${date_created?.getHours()}:${date_created?.getMinutes()}` }</div>
                 </div>
                 <hr />
             </Col>
